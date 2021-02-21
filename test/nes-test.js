c29d1fd48db0c4fe3a5bcbf0fb984ff649406dbd
szq <469763209@qq.com>
2021-02-21 21:59:22 +0800

    improve logger

--- test/nes-test.js
+++ test/nes-test.js
@@ -23,6 +23,67 @@ const fc = new Machine({
 fc.loadRom(romString)
 console.log(fc.gameRom.header)
 
+function operateWithLog() {
+  const to16pad4 = num => num.toString(16).toUpperCase().padStart(4, "0")
+  const to16pad2 = num => num.toString(16).toUpperCase().padStart(2, "0")
+
+  const currLogLine = []
+
+  // record cpu reg
+  const cReg = fc.cpu.reg
+  const flagByte = fc.cpu.flags2byte()
+  try {
+    currLogLine.push(
+      `${operateWithLog.lino++} ` +
+      `PC:${to16pad4(cReg.pc)} ` +
+      `A:${to16pad2(cReg.a)} ` +
+      `X:${to16pad2(cReg.x)} ` +
+      `Y:${to16pad2(cReg.y)} ` +
+      `P:${to16pad2(flagByte)} ` +
+      `SP:${to16pad2(cReg.s)}`
+    )
+  } catch (e) {
+    console.log(cReg)
+    console.log(flagByte)
+    throw e
+  }
+  // record ppu reg
+  const ppu = fc.ppu
+  const pReg = fc.cpuAddrSpace.ppuReg.innerBytes
+  try {
+    currLogLine.push(
+      `ppuAddrStep:${ppu.ppuAddrStep} ` +
+      `vramPointer:${isNaN(ppu.vramPointer) ? ppu.vramPointer : to16pad4(ppu.vramPointer)} ` +
+      `bufferedByte:${isNaN(ppu.bufferedByte) ? ppu.bufferedByte : to16pad2(ppu.bufferedByte)}\n\t` +
+      `0PPUCTRL:${to16pad2(pReg[0])} ` +
+      `1PPUMASK:${to16pad2(pReg[1])} ` +
+      `2PPUSTATUS:${to16pad2(pReg[2])} ` +
+      `3OAMADDR:${to16pad2(pReg[3])} ` +
+      `4OAMDATA:${to16pad2(pReg[4])} ` +
+      `5PPUSCROLL:${to16pad2(pReg[5])} ` +
+      `6PPUADDR:${to16pad2(pReg[6])} ` +
+      `7PPUDATA:${isNaN(pReg[7]) ? pReg[7] : to16pad2(pReg[7])} `
+    )
+  } catch (e) {
+    console.log(pReg)
+    throw e
+  }
+
+  let err = null
+  try {
+    fc.cpu.operate()
+  } catch (e) {
+    err = e
+    currLogLine[0] = "error: \"" + e + "\"\n" + currLogLine[0]
+  }
+  currLogLine.push(cpuLog.splice(0).join("  "))
+  ppuAddrSpaceLog.length && currLogLine.push(ppuAddrSpaceLog.splice(0).join("\n\t"))
+  cpuAddrSpaceLog.length && currLogLine.push(cpuAddrSpaceLog.splice(0).join("\n\t"))
+  log.push(currLogLine.join("\n\n\t"))
+  if (err !== null) throw err
+}
+operateWithLog.lino = 1
+
 
 function getMemWithLino(mem, numPad, startLino = 0) {
   const linoed = []
@@ -31,7 +92,7 @@ function getMemWithLino(mem, numPad, startLino = 0) {
     const byte = mem[i]
     const lino = (i + startLino).toString(16).padStart(numPad, "0")
     if (typeof (byte) === "undefined") {
-      linoed.push(`${lino}: undefined`)
+      linoed.push(`${lino}:`)
     } else {
       linoed.push(`${lino}: ${byte.toString(16).padStart(2, "0")}`)
     }
@@ -48,7 +109,7 @@ function getRamList() {
     },
     layout: (make, view) => {
       make.height.equalTo(view.super)
-      make.width.equalTo(150)
+      make.width.equalTo(100)
       make.left.top.equalTo(view.super)
     },
 
@@ -64,7 +125,7 @@ function getVRamNameTableList() {
     },
     layout: (make, view) => {
       make.height.equalTo(view.super)
-      make.width.equalTo(160)
+      make.width.equalTo(110)
       make.right.top.equalTo(view.super)
     },
 
@@ -80,7 +141,7 @@ function getVRamvPaletteList() {
     },
     layout: (make, view) => {
       make.height.equalTo(view.super)
-      make.width.equalTo(160)
+      make.width.equalTo(110)
       make.top.equalTo(view.super)
       make.right.equalTo($("vramName").left)
     },
@@ -107,6 +168,11 @@ function getOperateButton() {
         let num = Number($("operateTimes").text)
         $("operateTimes").blur()
 
+        $cache.setAsync({
+          key: "opTimes",
+          value: num
+        })
+
         if (!num) num = 1
         while (num--) {
           operateWithLog()
@@ -121,7 +187,8 @@ function getOperateTimesInput() {
     type: "input",
     props: {
       id: "operateTimes",
-      type: $kbType.number
+      type: $kbType.number,
+      text: $cache.get("opTimes")
     },
     layout: (make, view) => {
       make.size.equalTo($size(100, 35))
@@ -214,13 +281,16 @@ function getPrintLogButton() {
     },
     layout: (make, view) => {
       make.size.equalTo($size(110, 40))
-      make.centerX.equalTo(view.super)
-      make.top.equalTo($("vramRef").bottom).offset(30)
+      make.top.equalTo($("vramRef"))
+      make.left.equalTo($("vramRef").right).offset(20)
     },
     events: {
       tapped: (sender) => {
         if (log.length === 0) return
-        console.log(log.slice(sender.info[0], sender.info[1] || undefined).join("\n"))
+        // console.log(log.slice(sender.info[0], sender.info[1] || undefined).join("\n"))
+        for (const line of log.slice(sender.info[0], sender.info[1] || undefined)) {
+          console.log(line)
+        }
       }
     }
   }
@@ -236,7 +306,7 @@ function getSaveLogButton() {
     layout: (make, view) => {
       make.size.equalTo($size(110, 40))
       make.centerX.equalTo(view.super)
-      make.top.equalTo($("printLog").bottom).offset(30)
+      make.top.equalTo($("vramRef").bottom).offset(30)
     },
     events: {
       tapped: function (sender) {
@@ -244,7 +314,7 @@ function getSaveLogButton() {
           text: "test-log.txt",
           handler: function (text) {
             const success = $file.write({
-              data: $data({ string: log.join('\n') }),
+              data: $data({ string: log.join('\n\n\n') }),
               path: text
             })
             if (success) {
@@ -299,30 +369,11 @@ function getClearVBlankButton() {
   }
 }
 
-operateWithLog.lino = 1
-function operateWithLog() {
-  const to16pad4 = num => num.toString(16).toUpperCase().padStart(4, "0")
-  const to16pad2 = num => num.toString(16).toUpperCase().padStart(2, "0")
-
-  const reg = fc.cpu.reg
-  const flagByte = fc.cpu.flags2byte()
-  // record reg
-  const currLogLine = []
-
-  currLogLine.push(
-    `${operateWithLog.lino++} PC:${to16pad4(reg.pc)} A:${to16pad2(reg.a)} X:${to16pad2(reg.x)} Y:${to16pad2(reg.y)} P:${to16pad2(flagByte)} SP:${to16pad2(reg.s)}`
-  )
-
-  fc.cpu.operate()
-
-  currLogLine.push(cpuLog.splice(0).join(" "))
-  currLogLine.push(ppuAddrSpaceLog.splice(0).join(" "))
-  currLogLine.push(cpuAddrSpaceLog.splice(0).join(" "))
-  log.push(currLogLine.join("\t"))
-}
-
 function nesTest() {
   $ui.render({
+    props: {
+      title: "Nes Test"
+    },
     views: [
       getRamList(),
       getVRamNameTableList(),
